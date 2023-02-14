175d4290e886394c594f4aa4e7b08f83720ea623
marcosbilobram <marcosbilobram@hotmail.com>
2023-02-13 23:50:47 -0300

    first commit

---
+++ src/main/java/br/com/fiap/Ondoor/OndoorApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Ondoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndoorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/Ondoor/OndoorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Ondoor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndoorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
