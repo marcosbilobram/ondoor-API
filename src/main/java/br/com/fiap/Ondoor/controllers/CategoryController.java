7711d60925c58e837998950864f0234e9c0d83c5
Rafaela Gomes <99452621+gsrafaela@users.noreply.github.com>
2023-03-13 21:13:04 -0300

    Add files via upload

--- src/main/java/br/com/fiap/Ondoor/controllers/CategoryController.java
+++ src/main/java/br/com/fiap/Ondoor/controllers/CategoryController.java
@@ -1,7 +1,12 @@
 package br.com.fiap.Ondoor.controllers;
 
+import br.com.fiap.Ondoor.entities.Category;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategoryController {
+
+    @GetMapping(value = "/category")
+    public Category show() { return new Category();}
 }
--- src/main/java/br/com/fiap/Ondoor/controllers/UserController.java
+++ src/main/java/br/com/fiap/Ondoor/controllers/UserController.java
@@ -1,7 +1,12 @@
 package br.com.fiap.Ondoor.controllers;
 
+import br.com.fiap.Ondoor.entities.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    @GetMapping(value = "/user")
+    public User show() { return new User();}
 }
